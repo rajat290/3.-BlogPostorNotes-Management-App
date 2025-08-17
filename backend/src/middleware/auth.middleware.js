36371ee6c09e27b91b86542337fecfee3fb62be5
rajat <rajat290@users.noreply.github.com>
2025-08-17 23:49:33 +0530

    api/notes tested running succesfull

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -4,9 +4,9 @@ import User from '../models/user.js';
 export const protect = async (req, res, next) => {
     let token;
 
-    if (req.headers.authorization?.startsWith('Bearer')) {
+    if (req.headers.authorization?.startsWith("Bearer")) {
         try {
-            token = req.headers.authorization.split(' ')[1]; // ✅ space ke basis pe split
+            token = req.headers.authorization.split(" ")[1]; // ✅ space ke basis pe split
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
             req.user = await User.findById(decoded.id).select('-password');
             return next(); // ✅ next call zaroori
