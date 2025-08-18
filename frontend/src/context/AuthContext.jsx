0b3c24224a5396688050892dc08cfea684932225
rajat <rajat290@users.noreply.github.com>
2025-08-18 11:28:09 +0530

    context added

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,22 @@
+import {createContext , useState, useEffect, Children} from 'react';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ Children}) => {
+    const [user, setUser] = useState(null);
+    const [token, setToken] = useState(localStorage.getItem("token") || null);
+
+    useEffect (() => {
+        if (token) {
+            localStorage.setItem("token", token);
+        } else {
+            localStorage.removeItem("token");
+        }
+    }, [token]);
+
+    return (
+        <AuthContext.Provider value={{ user, setUser, token, setToken }}>
+            {Children}
+        </AuthContext.Provider>
+    );
+}
